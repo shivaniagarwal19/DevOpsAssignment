189adfa1c229aab63b1385e8433c09153beea0c8
Shivani Agarwal <43836101+shivaniagarwal19@users.noreply.github.com>
2021-11-29 12:24:31 +0530

    Assignment Solution

---
+++ DevProject/src/main/java/com/hcl/spring/DevProjectApplication.java
@@ -0,0 +1,15 @@
+package com.hcl.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevProjectApplication.class, args);
+		
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
---
+++ DevProject/src/test/java/com/hcl/spring/DevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
